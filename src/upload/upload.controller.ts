b96df7c3e4ed2c2b88e29b78ae4c0e37fba37151
cher1shRXD <taewoo080401@gmail.com>
2024-07-10 17:09:40 +0900

    fix::file upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
+import { FileDto } from './dto/file.dto'; // Ensure this matches your DTO structure
+import { Express } from 'express'; // Ensure Express is imported
 
 @Controller('upload')
 export class UploadController {
-  @Post('/upload')
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post('/')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<{ url: string }> {
+    return this.uploadService.saveFile(file);
   }
 }
